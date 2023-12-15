290b1532bc7b0f8fea9329e620c8b6196aff0153
tharusha1004 <tharusha.astro@gmail.com>
2023-12-15 10:48:39 +0530

    add career page and add construction image

--- frontend/src/pages/Career.jsx
+++ frontend/src/pages/Career.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import '../styles/Career.css'
+
+import bg_img from '../assets/images/pages_images/Construction_Bg.jpg'
 
 const Career = () => {
     return (
         <div className="career-container">
-            
+            <img className='image-style' src={bg_img} alt='' />
         </div>
     )
 }
