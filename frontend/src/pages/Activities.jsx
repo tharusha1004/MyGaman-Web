307457b055f19feb76f8b8c214d2819a3ecc4801
tharusha1004 <tharusha.astro@gmail.com>
2023-12-18 11:44:47 +0530

    add banner to the activities page

--- frontend/src/pages/Activities.jsx
+++ frontend/src/pages/Activities.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import '../styles/Activities.css'
+
+import bg_img from '../assets/images/pages_images/Construction_Bg.jpg'
+
 
 const Activities = () => {
     return (
-        <div>Activities</div>
+        <div className="activities-container">
+            <h1 className='custom-heading'>Welcome to Activities Page</h1>
+            <img className='image-style' src={bg_img} alt='' />
+        </div>
     )
 }
 
