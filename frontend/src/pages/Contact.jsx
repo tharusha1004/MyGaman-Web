ee51255fa67ee4e3266f01769d23754b55991e23
tharusha1004 <tharusha.astro@gmail.com>
2023-12-18 11:58:40 +0530

    add banner to contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import '../styles/Contact.css'
+
+import bg_img from '../assets/images/pages_images/Construction_Bg.jpg'
+
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className="contact-container">
+            <h1 className='custom-heading'>Welcome to Contact Page</h1>
+            <img className='image-style' src={bg_img} alt='' />
+        </div>
     )
 }
 
