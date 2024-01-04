8eff63e79397d19eeb1b2a6983e476044f394155
tharusha1004 <tharusha.astro@gmail.com>
2024-01-04 15:33:40 +0530

    add main section component to the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../styles/Home.css'
 import InfoMessage from '../components/Home/Info_Message/InfoMessage'
+import MainSection from '../components/Home/Main_Section/MainSection'
 
 const Home = () => {
     return (
         <div>
             <InfoMessage />
+            <MainSection/>
         </div>
     )
 }
