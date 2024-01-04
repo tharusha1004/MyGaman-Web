e85abb9008997dbf4479d4cdf84a95c1ed1d975b
tharusha1004 <tharusha.astro@gmail.com>
2024-01-04 15:32:49 +0530

    create main component of the home page

---
+++ frontend/src/components/Home/Main_Section/MainSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../Main_Section/MainSection.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const MainSection = () => {
+    return (
+        <section className="px-5">
+            <div class="text-white Image01 col-sm-12 col-md-12 col-lg-12">
+                <div class="container">
+                    <div class="row text-center">
+                        <div className=" mx-auto d-block">
+                            <p className="text03">Visit Sri Lanka, The Pearl of Indian Ocean</p>
+                            <p class="text04">"Discover the Timeless Beauty and Rich Heritage of Sri Lanka:</p>
+                            <p class="text04">Your Gateway to Serenity and Adventure Awaits!"</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default MainSection
