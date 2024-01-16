8bfcc3db343bd7fa806fc733bf20b23411b6fc94
tharusha1004 <tharusha.astro@gmail.com>
2024-01-16 13:17:24 +0530

    add search bar with validation in main page

---
+++ frontend/src/shared/SearchBar.jsx
@@ -0,0 +1,55 @@
+import React, { useRef } from 'react';
+import '../shared/SearchBar.css';
+import { Col, Form, FormGroup } from "reactstrap"
+
+const SearchBar = () => {
+
+    const locationRef = useRef('')
+    const distanceRef = useRef(0)
+    const maxGroupSizeRef = useRef(0)
+
+    const searchHandler = () => {
+        const location = locationRef.current.value
+        const distance = distanceRef.current.value
+        const maxGroupSize = maxGroupSizeRef.current.value
+
+        if (location === '' || distance === '' || maxGroupSize === '' ) {
+            return alert ("All fields are required!");
+        }
+    }
+
+    return (
+        <Col lg='12'>
+            <div className="search__bar">
+                <Form className="d-flex align-items-center gap-4">
+                    <FormGroup className="d-flex gap-3 form__group form__group-fast">
+                        <span><i class="ri-map-pin-line"></i></span>
+                        <div>
+                            <h6>Location</h6>
+                            <input type="text" placeholder='Where are your going?' ref={locationRef} />
+                        </div>
+                    </FormGroup>
+                    <FormGroup className="d-flex gap-3 form__group form__group-fast">
+                        <span><i class="ri-map-pin-time-line"></i></span>
+                        <div>
+                            <h6>Distance</h6>
+                            <input type="number" placeholder='Distance (km)' ref={distanceRef} />
+                        </div>
+                    </FormGroup>
+                    <FormGroup className="d-flex gap-3 form__group form__group-last">
+                        <span><i class="ri-group-line"></i></span>
+                        <div>
+                            <h6>Max People</h6>
+                            <input type="number" placeholder='0' ref={maxGroupSizeRef} />
+                        </div>
+                    </FormGroup>
+                    <span className='search__icon' type='submit' onClick={searchHandler}>
+                        <i class="ri-search-line"></i>
+                    </span>
+                </Form>
+            </div>
+        </Col>
+    )
+}
+
+export default SearchBar
