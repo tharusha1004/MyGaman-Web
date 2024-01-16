4f6a8c0593756fdfd91d842e380ddbd86d947271
tharusha1004 <tharusha.astro@gmail.com>
2024-01-16 13:17:49 +0530

    add search bar component into main page.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,14 @@ import '../styles/Home.css'
 import InfoMessage from '../components/Home/Info_Message/InfoMessage'
 import MainSection from '../components/Home/Main_Section/MainSection'
 
+import SearchBar from '../shared/SearchBar'
+
 const Home = () => {
     return (
         <div>
             <InfoMessage />
             <MainSection/>
+            <SearchBar/>
         </div>
     )
 }
