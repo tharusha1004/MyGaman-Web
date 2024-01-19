ce3b05925ee913b86c898c09c384766196d5caed
tharusha1004 <tharusha.astro@gmail.com>
2024-01-19 12:14:57 +0530

    add package component into home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import SearchBar from '../shared/SearchBar'
 import DestinationSection from '../components/Home/Destination_Section/DestinationSection'
 import ActivityBooking from '../components/Home/Activity_Booking/ActivityBooking'
 import ServicesSection from '../components/Home/Services_Section/ServicesSection'
+import PackagesSection from '../components/Home/Packages_Section/PackagesSection'
 
 const Home = () => {
     return (
@@ -17,6 +18,7 @@ const Home = () => {
             <DestinationSection/>
             <ActivityBooking/>
             <ServicesSection/>
+            <PackagesSection/>
         </div>
     )
 }
